67c94e370ac463fd0b187dd6772026b46b55b9e0
Hector Cano <hectorcanomaestre@hotmail.com>
2020-11-12 23:44:50 +0100

    Números aleatorios del 10 al 50 ambos incluidos

--- src/hectoradivino/Aciertos.java
+++ src/hectoradivino/Aciertos.java
@@ -6,7 +6,7 @@ public class Aciertos {
 
     public static void main(String[] args) {
         int n = 0,cont = 0;
-        int x = (int) (Math.random() * (51 - 10) + 10);
+        int x = (int) (Math.random() * (51 - 10) + 10);// Devuelve un número aleatorio entre min (incluido) y max (excluido)
         //System.out.print(x);
         Scanner sc = new Scanner(System.in);
         do {
